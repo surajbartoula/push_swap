f5df48967e08b2a9cbd244f79d281982f5c56e68
surajbartoula <suraj_bartoula@yahoo.com>
2024-04-27 15:30:03 +0400

    3 number is working now

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 22:46:33 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/04/27 14:36:32 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/04/27 15:29:10 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,29 @@
 
 void	ft_tiny_sort(t_stack **a)
 {
-	t_stack	*first;
-	t_stack	*middle;
-	t_stack	*last;
+	t_stack	*f;
+	t_stack	*m;
+	t_stack	*l;
 
-	first = *a;
-	middle = (*a)->next;
-	last = (*a)->next->next;
-	if (middle->p_index > first->p_index && middle->p_index > last->p_index
-		&& first->p_index < last->p_index)
+	f = *a;
+	m = (*a)->next;
+	l = (*a)->next->next;
+	if (m->num > f->num && m->num > l->num && f->num < l->num)
 	{
 		ft_rra(a, 1);
 		ft_sa(a, 1);
 	}
-	else if (middle->p_index < first->p_index && middle->p_index < last->p_index
-		&& first->p_index < last->p_index)
+	else if (m->num < f->num && m->num < l->num && f->num < l->num)
 		ft_sa(a, 1);
-	else if (middle->p_index > first->p_index && middle->p_index > last->p_index
-		&& first->p_index > last->p_index)
+	else if (m->num > f->num && m->num > l->num && f->num > l->num)
 		ft_rra(a, 1);
-	else if (middle->p_index < first->p_index && middle->p_index < last->p_index
-		&& first->p_index > last->p_index)
+	else if (m->num < f->num && m->num < l->num && f->num > l->num)
 		ft_ra(a, 1);
-	else if (middle->p_index < first->p_index
-		&& middle->p_index > last->p_index)
-		ft_rra(a, 1);
+	else if (m->num < f->num && m->num > l->num)
+	{
+		ft_ra(a, 1);
+		ft_sa(a, 1);
+	}
 }
 
 void	ft_sort(t_stack **a)
