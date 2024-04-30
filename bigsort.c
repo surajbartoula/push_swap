bccee6f551df549b3662fdc988480e745e773fd0
surajbartoula <suraj_bartoula@yahoo.com>
2024-04-30 14:51:00 +0000

    optimized version added

--- bigsort.c
+++ bigsort.c
@@ -6,11 +6,18 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 05:58:03 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/04/29 20:21:01 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/04/30 07:51:24 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	anticlock(int index, int size)
+{
+	if (index <= (size - index))
+		return (1);
+	return (0);
+}
 
 void	anticlockwise(t_stack **a, t_stack **b, int index)
 {
@@ -24,28 +31,44 @@ void	clockwise(t_stack **a, t_stack **b, int index, int size)
 	ft_pb(a, b, 1);
 }
 
+void	rotatestack(t_stack **a, t_stack **b, int index, int size)
+{
+	if (anticlock(index, size))
+		anticlockwise(a, b, index);
+	else
+		clockwise(a, b, index, size);
+}
+
+int	indexposition(t_stack **a, int i)
+{
+	t_stack	*current;
+	int		index;
+
+	index = 1;
+	while (current && current->p_index != i)
+	{
+		current = current->next;
+		index++;
+	}
+	return (index);
+}
+
 void	ft_rotate_push(t_stack **a, t_stack **b, int len)
 {
 	int		i;
-	int		index;
+	int		findex;
+	int		sindex;
 	int		size;
 	t_stack	*current;
 
 	i = 1;
 	while (i < len - 2)
 	{
-		index = 1;
 		current = *a;
 		size = ft_lstsize(current);
-		while (current && current->p_index != i)
-		{
-			current = current->next;
-			index++;
-		}
-		if (index <= (size - index))
-			anticlockwise(a, b, index);
-		else
-			clockwise(a, b, index, size);
+		findex = indexposition(a, i);
+		sindex = indexposition(a, i + 1);
+		rotatestack(a, b, findex, size);
 		i++;
 	}
 }
